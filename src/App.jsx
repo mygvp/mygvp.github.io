280fcc68e7adb0ddd5b66d3f6569977bfec72769
kumarram27 <115088025+kumarram27@users.noreply.github.com>
2024-08-05 18:44:21 +0530

    chore: Remove unused code and update component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
-import Header from "./components/Header";
-import Results from "./components/Results";
-import Hero from "./components/Hero";
-import FeaturesSectionDemo from "./components/design/Features";
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Header />
-      {/* <Hero /> */}
-      <Results />
-      </div>
-      <ButtonGradient />
+      Hello World
     </>
   );
 };
