dc8ca82d8fa7c3ff56f1f68ffeabd3b0c6ac026e
kumarram27 <115088025+kumarram27@users.noreply.github.com>
2024-08-13 00:12:31 +0530

    first commit

---
+++ src/components/navigation/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='px-4 h-14 sticky top-0 inset-x-0 w-full bg-background/40  backdrop-blur-lg  border '> Navbar</header>
+  )
+}
+
+export default Navbar
